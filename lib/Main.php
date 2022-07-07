bca5d5ab54b3eb1c52377ae79e63137592e919b6
kenta1996 <1234567890kentatomo@gmail.com>
2022-07-07 18:24:14 +0900

    BlackJack公開

--- lib/Main.php
+++ lib/Main.php
@@ -5,6 +5,8 @@ namespace BlackJack5;
 require_once('BlackJackGame.php');
 require_once('User.php');
 
+// ゲーム開始
+// php lib/Main.php 
 $player = new User('you');
 $cp1 = new User('CP1');
 $cp2 = new User('CP2');
